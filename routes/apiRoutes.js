26f9b002cd094211404d0d1b188e9e4efafa8c73
Vivi Cowan <vivicare@gmail.com>
2021-07-18 20:43:23 -0700

    created api routes and delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-const notesMade = renderNotes();
+let notesMade = getNotes();
 
-function renderNotes() {
+function getNotes() {
 	let data = fs.readFileSync("./db/db.json", "UTF-8");
 	let notes = JSON.parse(data);
 
@@ -13,3 +13,40 @@ function renderNotes() {
 	}
 	return notes;
 }
+
+module.exports = function (app) {
+	app.get("/api/notes", (req,res) =>{
+      notesMade = getNotes();
+      //sends JSON response by converting noteData to JSON string using JSON stringfy() method
+      res.json(notesMade);
+  });
+
+  app.get("api/notes", (req,res)=>{
+    // console.log(req.body);
+    notesMade.push(req.body);
+    res.json(true);
+    
+  });
+
+  app.post("/api/notes", (req,res) =>{
+    notesMade.push(req.body);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesMade), "UTF-8");
+    res.json(true);
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    const newID = req.params.id;
+
+    let note = notesMade.filter(note => {
+      return note.id === newID;
+    })[0];
+
+    const idIndex = notesMade.indexOf(note);
+
+    notesMade.splice(idIndex, 1);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesMade), "utf8");
+    res.json("Note deleted");
+  });
+ };
+ 
