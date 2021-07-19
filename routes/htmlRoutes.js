49ee0b8ad993ea68a25dd76871d1c1abe90ec7a5
Vivi Cowan <vivicare@gmail.com>
2021-07-18 18:03:11 -0700

    fixed "export" to "exports"

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 // exports and accepts express app object and sets up routes
-module.export = (app) => {
+module.exports = function(app) {
 	// when "Get Started" button is clicked, the response creates the path which takes the client to notes.html
 	app.get("/notes", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/notes.html"));
