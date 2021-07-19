d0b6a6dcc9c6ca3c9bdb7d9503f224380bbeac0a
Vivi Cowan <vivicare@gmail.com>
2021-07-18 17:48:04 -0700

    added comments explaining routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,14 @@
+// path package allows us to construct paths to html files
 const path = require("path");
 
+// exports and accepts express app object and sets up routes
 modules.export = (app) => {
+	// when "Get Started" button is clicked, the response creates the path which takes the client to notes.html
 	app.get("/notes", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/notes.html"));
 	});
 
+	// default get request route
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '../public/index.html'));
 	 });
