7ca382414803cddf553aa715a812c9742cfc4673
Vivi Cowan <vivicare@gmail.com>
2021-07-18 18:31:45 -0700

    created function to display notes made in db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,15 @@
 const fs = require("fs");
 const path = require("path");
+
+const notesMade = renderNotes();
+
+function renderNotes() {
+	let data = fs.readFileSync("./db/db.json", "UTF-8");
+	let notes = JSON.parse(data);
+
+	for (let i = 0; i < notes.length; i++) {
+		notes[i].id = "" + i;
+		console.log(notes);
+	}
+	return notes;
+}
